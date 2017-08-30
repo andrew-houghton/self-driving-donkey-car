13f55c4052a795eda2344920fb3c62381a98ad18
Chester Wang <gwan8849@uni.sydney.edu.au>
2017-08-31 01:23:47 +1000

    Added the keras implementation of AlexNet, ZFNet and VGGNet

---
+++ GengxingWang_SourceCode_of_NN_Models_and_Image_preprocessing/AlexNet.py
@@ -0,0 +1,44 @@
+# https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.layers import merge
+from keras.layers.normalization import BatchNormalization
+
+def AlexNet():
+
+    model = Sequential()
+    model.add(Conv2D(64, 3, 11, 11, border_mode='full'))
+    model.add(BatchNormalization((64,226,226)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(poolsize=(3, 3)))
+
+    model.add(Conv2D(128, 64, 7, 7, border_mode='full'))
+    model.add(BatchNormalization((128,115,115)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(poolsize=(3, 3)))
+
+    model.add(Conv2D(192, 128, 3, 3, border_mode='full'))
+    model.add(BatchNormalization((128,112,112)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(poolsize=(3, 3)))
+
+    model.add(Conv2D(256, 192, 3, 3, border_mode='full'))
+    model.add(BatchNormalization((128,108,108)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(poolsize=(3, 3)))
+
+    model.add(Flatten())
+    model.add(Dense(12*12*256, 4096, init='normal'))
+    model.add(BatchNormalization(4096))
+    model.add(Activation('relu'))
+    model.add(Dense(4096, 4096, init='normal'))
+    model.add(BatchNormalization(4096))
+    model.add(Activation('relu'))
+    model.add(Dense(4096, 1000, init='normal'))
+    model.add(BatchNormalization(1000))
+    model.add(Activation('softmax'))
+
+    return model
---
+++ GengxingWang_SourceCode_of_NN_Models_and_Image_preprocessing/VGGNet.py
@@ -0,0 +1,57 @@
+# https://arxiv.org/pdf/1409.1556.pdf
+
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+
+
+def VGG_16():
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Conv2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000, activation='softmax'))
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
